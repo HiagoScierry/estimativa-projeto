4d15c0e56e3763ccfcd5d9bb5155d558e3979990
Hiago Moreira <hiagocarlosmoreira@hotmail.com>
2025-03-17 01:44:00 -0300

    remoção de projeto

--- src/main/java/br/projeto/command/ExcluirProjetoProjetoCommand.java
+++ src/main/java/br/projeto/command/ExcluirProjetoProjetoCommand.java
@@ -1,6 +1,7 @@
 package br.projeto.command;
 
 import br.projeto.model.Projeto;
+import br.projeto.repository.ProjetoRepository;
 import br.projeto.singleton.ProjetoSingleton;
 
 import javax.swing.*;
@@ -8,21 +9,23 @@ import javax.swing.*;
 public class ExcluirProjetoProjetoCommand implements ProjetoCommand {
     private ProjetoSingleton projetoSingleton;
     private Projeto projeto;
+    private ProjetoRepository projetoRepository;
 
     public ExcluirProjetoProjetoCommand(ProjetoSingleton projetoSingleton) {
         this.projetoSingleton = projetoSingleton;
+        this.projetoRepository = new ProjetoRepository();
     }
 
     public ExcluirProjetoProjetoCommand(ProjetoSingleton projetoSingleton, Projeto projeto) {
         this.projetoSingleton = projetoSingleton;
         this.projeto = projeto;
+        this.projetoRepository = new ProjetoRepository();
     }
 
 
     public void setProjeto(Projeto projeto) {
         this.projeto = projeto;
     }
-
 
     @Override
     public void execute() {
@@ -40,7 +43,9 @@ public class ExcluirProjetoProjetoCommand implements ProjetoCommand {
 
         if (confirmacao == JOptionPane.YES_OPTION) {
             boolean removido = projetoSingleton.removerProjetoPorId(projeto.getId());
+            projetoRepository.removerProjeto(projeto.getId());
             if (removido) {
+
                 new MostrarMensagemProjetoCommand("Projeto \"" + projeto.getNome() + "\" removido com sucesso!").execute();
             } else {
                 new MostrarMensagemProjetoCommand("Erro ao remover o projeto \"" + projeto.getNome() + "\".").execute();
